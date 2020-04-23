2209f0ae86f0db079575f0c00a785ce6a8c3d6fc
Ben Richardson <benjamin.c.richardson.0@gmail.com>
2020-04-23 15:24:12 -0500

    Clean up styles for mobile.

--- components/ExplainInsight.vue
+++ components/ExplainInsight.vue
@@ -1,5 +1,6 @@
 <template>
     <v-popover
+    style="display: inline-block"
     offset="16"
     placement="top-start"
     >
@@ -12,7 +13,7 @@
             Arturo provides AI derived insights in the form of physical property characteristics. Property Characteristics relate to distinct features of a property (i.e., physically bounded spatial area largely defined by the legal parcel) derived from running our deep learning models on aerial and satellite imagery.
         </p>
         <p>
-            Each insight includes characteristics such as number of buildings, roof type & area, roof material, roof complexity & condition, pools, trampolines, cars, number of stories, building/tree overlap, and much more.
+            Each insight includes characteristics such as number of buildings, roof type &amp; area, roof material, roof complexity &amp; condition, pools, trampolines, cars, number of stories, building/tree overlap, and much more.
         </p>
         <p class="b-0">
             You can <a v-on:click="show" v-close-popover>request a demo</a> to learn more about all of the insights we offer.
@@ -48,14 +49,24 @@ export default {
         }
     }
     .info-icon {
-        font-weight: bold;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        height: 1.75rem;
+        width: 1.75rem;
+        border-radius: 50%;
+        border: 2px solid $color_white;
         background-color: $color_darkBlue;
         color: $color_white;
-        border: 2px solid $color_white;
-        height: 2rem;
-        width: 2rem;
-        border-radius: 50%;
+
+        font-weight: 600;
+        font-size: .95rem;
+        line-height: 1;
+
+        -webkit-appearance: none;
         outline: none;
+
         transition: all 0.3s;
         &:hover{
             cursor: pointer;
